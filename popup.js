ae6d393eef890e0a570e6d3562671b20a8ca77b8
Jonah Fried <jahfried@gmail.com>
2020-10-15 17:48:06 -0400

    Added poetry to type selector

--- popup.js
+++ popup.js
@@ -13,6 +13,13 @@ window.addEventListener("DOMContentLoaded", function () {
     var quoteFinder = document.getElementById("findQuotes");
 
     quoteFinder.addEventListener("click", searchPage);
+
+    document.getElementById("userInput").addEventListener("keydown", function (e) {
+        if (e.key == "Enter" && e.metaKey) {
+            e.preventDefault();
+            searchPage(this.value);
+        }
+    })
 });
 
 function activateHelpButton() {
@@ -40,6 +47,7 @@ function handleLocalStore() {
     if (localStorage["text"] != undefined) {
         // First we disable the findQuotes button as it isn't necessary 
         $("#input-wrapper").addClass("clear");
+        $("#quoteTypeForm").addClass("clear");
         queryQuotes(localStorage["text"]);
         $("body").css("width", "");
         localStorage.clear();
@@ -161,74 +169,3 @@ function displayQuery(d) {
     $("#loading").addClass("clear");
 
 }
-
-
-// function displayQuery(d) {
-//     // Get the table from the popup page
-//     var table = $("#displayTable");
-
-//     let sentences = d.sentences;
-//     // console.log(sentences);
-
-//     if (sentences.length > 0) {
-//         for (let i = 0; i < sentences.length; i++) {
-//             let sentence = sentences[i].sentence
-//             if (sentence == "") {
-//                 continue;
-//             }
-//             // We skip quotes that are too long because they break 
-//             // the table css
-//             if (sentence.length > MAX_QUOTE_LEN) { // This number is just a guess quick fix
-//                 continue;
-//             }
-//             let source = sentences[i].title;
-//             let sourceLink = $(sentences[i].linked_title);
-//             sourceLink.on("click", function () {
-//                 chrome.tabs.create({ url: $(this).attr("href") });
-//                 return false;
-//             });
-
-//             // Create the table row 
-//             let row = $("<tr></tr>");
-//             let sentenceCell = $("<td></td>");
-//             sentenceCell.append(sentence);
-
-//             // Create copy line button that writes to sys clipboard
-//             // let popoverInfo = "data-toggle='popover' data-content='copied!' data-trigger='focus' data-placement='top'"
-//             let copyButton = $("<button data-toggle='popover'></button>");
-//             copyButton.append(String.fromCodePoint("0x1f4cb"));
-//             copyButton.addClass("btn");
-//             copyButton.on("click", function () {
-//                 let quotedSentence = '"' + sentence + '" ';
-//                 let citation = "(" + source + ")";
-//                 navigator.clipboard.writeText(quotedSentence + citation);
-//             });
-//             let copyCell = $("<td></td>");
-//             copyCell.append(copyButton);
-
-//             // Extract and create cell for author 
-//             let sourceCell = $("<td></td>");
-//             sourceCell.append(sourceLink);
-
-//             //  Append data to the row and to the table
-//             row.append(copyCell);
-//             row.append(sentenceCell);
-//             row.append(sourceCell);
-//             table.append(row);
-
-//         }
-
-//         $('[data-toggle="popover"]').popover({ content: "copied!", animation: true, placement: "top", trigger: "focus" });
-
-//         table.removeClass("clear");
-//     } else {
-//         $("#noResults").removeClass("clear")
-//     }
-//     // Now that all of the copyButtons have been created
-//     // we can activate their bootstrap popover functionality
-
-
-//     // Get the loading message to hide it
-//     $("#loading").addClass("clear");
-
-// }
